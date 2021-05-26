948684fed5aec86c8358fdd96822401555cd35b4
Bilal-Bangash <muhammadbilalbangash@hotmail.com>
2021-05-26 15:06:49 +0500

    Fetch Products from Database

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import products from "./data/products.js";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
+import { productRoutes } from "./routes/index.js";
 
 dotenv.config();
 connectDB();
@@ -11,13 +12,8 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("API is running");
 });
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((item) => item._id === req.params.id);
-  res.json(product);
-});
+
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
