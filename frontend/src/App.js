779800f402449641c49426ff5451c5c57b887462
Bilal-Bangash <muhammadbilalbangash@hotmail.com>
2021-05-28 11:57:43 +0500

    Qty Select & Add to Cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Header, Footer } from "./components";
-import { HomeScreen, ProductScreen } from "./screens";
+import { HomeScreen, ProductScreen, CartScreen } from "./screens";
 const App = () => {
   return (
     <Router>
@@ -12,6 +12,7 @@ const App = () => {
           <Switch>
             <Route exact path="/" component={HomeScreen} />
             <Route exact path="/product/:id" component={ProductScreen} />
+            <Route exact path="/cart/:id?" component={CartScreen} />
           </Switch>
         </Container>
       </main>
